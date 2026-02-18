4778a5f42b1bcd32f27e49a1c91bb24c37408a61
Randonneur1 <amahov3@gmail.com>
2026-02-18 08:57:02 +0300

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fastfetch
+end
+alias u="sudo pacman -Syu"
+alias U="yay -Syu"
+alias c="clear"
+alias f="fastfetch"
+alias q="exit"
+alias p="reboot"
+alias P="poweroff"
